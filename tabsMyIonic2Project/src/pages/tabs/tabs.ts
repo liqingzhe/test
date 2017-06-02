cef993038ec62525477ebd9a7494c254492da6f7
liqing <liqing_0908@163.com>
2017-06-02 15:10:44 +0800

    修复按钮bug layer晃动

--- tabsMyIonic2Project/src/pages/tabs/tabs.ts
+++ tabsMyIonic2Project/src/pages/tabs/tabs.ts
@@ -22,20 +22,4 @@ export class TabsPage {
 
   }
 
-  iconArray: Array<string> = ["iconfont-home","iconfont-house","iconfont-gongyu","iconfont-life"];
-  change(a: number) {
-  	for (let i = 0; i < 4; i++) {
-  		if (i === a) {
-  			if(this.iconArray[i].indexOf('outline') > 0){
-  				let array1 = this.iconArray[i].split("-");
-  				let array2 = [array1[0],array1[1]].join('-');
-  				this.iconArray[i] = array2;
-  			}
-  			this.iconArray[i] = this.iconArray[i];
-  		}
-  		else if(i != a && this.iconArray[i].indexOf('outline') < 0){
-  			this.iconArray[i] = this.iconArray[i] + "-outline";
-  		} 
-  	} 
-  }
 }
